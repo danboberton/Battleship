c6ecd2485c004c39d81cfaf10d0a4a81ac145595
elitadanilyuk <jelitadanilyuk@gmail.com>
2021-04-12 19:09:27 -0600

    I forgot class Player, here it is.

---
+++ Player.cpp
@@ -0,0 +1,11 @@
+/*
+ * This is Battleship
+ *
+ * @author Dan Butcher & Elita Danilyuk
+ * @version2021-04-12
+ */
+
+#include "Player.h"
+#include <string>
+#include <iostream>
+using namespace std;
---
+++ Player.h
@@ -0,0 +1,19 @@
+/*
+ * This is Battleship
+ *
+ * @author Dan Butcher & Elita Danilyuk
+ * @version2021-04-12
+ */
+
+#include <string>
+#pragma once
+using namespace std;
+
+namespace CSC234{
+    class Player{
+    public:
+
+    private:
+
+    }; //end class Player
+} //end namespace CSC234
